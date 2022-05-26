dc78d7bec268455199c9c34c3c83f7a6e3e9ef22
sourtaste000 <sourtaste000@i-sp.in>
2022-05-26 15:30:38 -0700

    add missing deps part 2 and partially fix index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const { Client, Collection, Permissions } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const { GatewayIntentBits } = require('discord-api-types/v10');
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
   ],
   partials: [
     'MESSAGE',
     'CHANNEL',
-    'GUILD_MEMBER',
   ],
 });
+
 require('dotenv').config();
 const fs = require('fs');
 
@@ -19,9 +21,11 @@ client.slash = new Collection();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  require('./slash')(client);
+
+  require('./slash')(client); // TODO: Fix this later
   require('./events')(client);
-  fs.readdirSync(`${__dirname}/\/commands/`).map(async (cmd) => {
+
+  fs.readdirSync(`${__dirname}/commands/`).map(async (cmd) => {
     const pull = require(`./commands/${cmd}`);
     client.slash.set(pull.name, pull);
     console.log(client.slash);
