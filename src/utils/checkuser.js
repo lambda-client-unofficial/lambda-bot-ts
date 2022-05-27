23b3758dd1c4c6cbce49a68917220ef2bcf023bb
i-spin <me@i-sp.in>
2022-05-26 20:34:16 -0700

    fix: lint checkuser.js

--- src/utils/checkuser.js
+++ src/utils/checkuser.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 
 const checkuser = async (id) => {
-  if (!id instanceof Number) return false;
-  const req = await axios.get(`https://discordapp.com/api/v9/users/${id}`, {
+  if (!(id instanceof Number)) return false;
+  axios.get(`https://discordapp.com/api/v9/users/${id}`, {
     headers: {
       authorization: `Bot ${process.env.TOKEN}`,
     },
-  }).then((req) => req.data.id).catch((err) => err);
-  return !(req instanceof Error);
+  }).then((req) => req.data.id).catch((_err) => false);
+  return true;
 };
 module.exports = checkuser;
