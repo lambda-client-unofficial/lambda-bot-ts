34cb640e143b2f9effc3093baac4013f3730e1ce
i-spin <me@i-sp.in>
2022-05-27 09:07:36 -0700

    build: add typescript build
    style: enable typescript linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,21 @@
 module.exports = {
   env: {
-    commonjs: true,
     es2021: true,
     node: true,
   },
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',e
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     '@typescript-eslint',
   ],
   rules: {
-    'import/extensions': 'off',
-    'no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
-    ],
   },
 };
