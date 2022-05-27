b046e7afd236c22bae3bf405d2bc3ff78963b988
i-spin <me@i-sp.in>
2022-05-26 20:35:32 -0700

    fix: lint minecraftProfile.js

--- src/utils/minecraftProfile.js
+++ src/utils/minecraftProfile.js
@@ -1,17 +1,17 @@
 const axios = require('axios').default;
 
 const profile = async (name) => {
-  const user = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${name}`).then((user) => user?.data?.id).catch((user) => user);
+  const user = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${name}`).then((res) => res?.data?.id).catch((err) => err);
   return user instanceof Error ? false : user;
 };
 const names = async (name) => {
-  const names = await axios.get(`https://api.mojang.com/user/profiles/${name}/names`).then((names) => names?.data).catch((names) => names);
-  return names instanceof Error ? false : names;
+  const data = await axios.get(`https://api.mojang.com/user/profiles/${name}/names`).then((res) => res?.data).catch((err) => err);
+  return data instanceof Error ? false : data;
 };
 const textures = async (name) => {
-  const req = await axios.get(`https://sessionserver.mojang.com/session/minecraft/profile/${name}`).then((textures) => textures?.data?.properties[0]?.value).catch((textures) => textures);
-  const textures = JSON.parse(Buffer.from(req, 'base64').toString()).textures?.SKIN?.url;
-  return textures instanceof Error ? false : textures;
+  const req = await axios.get(`https://sessionserver.mojang.com/session/minecraft/profile/${name}`).then((res) => res?.data?.properties[0]?.value).catch((err) => err);
+  const data = JSON.parse(Buffer.from(req, 'base64').toString()).textures?.SKIN?.url;
+  return data instanceof Error ? false : data;
 };
 const modules = {
   profile,
