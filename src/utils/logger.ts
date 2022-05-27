6f2bcfd7affa1e64843ab222ad0de563bcaae03e
i-spin <me@i-sp.in>
2022-05-27 09:22:19 -0700

    feat: logging system

---
+++ src/utils/logger.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import colors from 'colors';
+
+const log = (message: string) => {
+  console.log(`${colors.gray(new Date().toLocaleString())} ${colors.cyan('[INFO]')} ${message}`);
+};
+
+const warn = (message: string) => {
+  console.warn(`${colors.gray(new Date().toLocaleString())} ${colors.yellow('[WARN]')} ${message}`);
+};
+
+const error = (message: string) => {
+  console.error(`${colors.gray(new Date().toLocaleString())} ${colors.red('[ERROR]')} ${message}`);
+};
+
+export {
+  log,
+  warn,
+  error,
+};
