b2b0b85f20f4643e7306647b2597ba6228bdcac7
i-spin <me@i-sp.in>
2022-05-28 21:20:46 -0700

    chore: remove extra method in uuid.ts

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 const format = (uuid: string) => {
   if (typeof uuid !== 'string') throw Error('Bad UUID');
   const part1 = uuid.slice(0, 8);
@@ -9,16 +7,9 @@ const format = (uuid: string) => {
   const part5 = uuid.slice(20, 32);
   return `${part1}-${part2}-${part3}-${part4}-${part5}`;
 };
-const usernameToUUID = (username: string) => {
-  fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((e) => (e));
-};
 
 const uuidUtils = {
   format,
-  usernameToUUID,
 };
 
 export default uuidUtils;
