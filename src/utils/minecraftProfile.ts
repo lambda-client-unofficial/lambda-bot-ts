86132bbf7d303973ba0400da68c2c5800d0a38f7
i-spin <me@i-sp.in>
2022-05-28 21:17:35 -0700

    feat: use interfaces for api calls in utils

--- src/utils/minecraftProfile.ts
+++ src/utils/minecraftProfile.ts
@@ -1,26 +1,33 @@
 import fetch from 'node-fetch';
+import Profile from '../interfaces/minecraftProfile';
+import Names from '../interfaces/minecraftNames';
+import Texture from '../interfaces/minecraftTexture';
 
-const profile = async (name: string) => {
+function profile(name: string): Profile | undefined {
   fetch(`https://api.mojang.com/users/profiles/minecraft/${name}`)
     .then((res) => res.json())
-    .then((data) => data)
-    .catch((err) => err);
-};
-const names = async (uuid: string) => {
+    .then((data) => data as Profile)
+    .catch(() => {});
+  return undefined;
+}
+
+function names(uuid: string): Names | undefined {
   fetch(`https://api.mojang.com/user/profiles/${uuid}/names`)
-    .then((res) => res.json)
-    .then((data) => data)
-    .catch((err) => err);
-};
-const textures = async (name: string) => {
+    .then((res) => res.json())
+    .then((data) => data as Names)
+    .catch(() => {});
+  return undefined;
+}
+function textures(name: string): string | undefined {
   fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${name}`)
     .then((res) => res.json())
-    .then((data: any) => {
-      const { value } = data.properties[0];
+    .then((data) => {
+      const { value } = (data as Texture).properties[0];
       return JSON.parse(Buffer.from(value, 'base64').toString()).textures?.SKIN?.url;
     })
-    .catch((err) => err);
-};
+    .catch(() => {});
+  return undefined;
+}
 
 const minecraftUtils = {
   profile,
