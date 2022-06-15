33349a41b0f4fb1a7eef594b14092b6db950acc8
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-06-15 19:04:58 -0400

    fix: Not loading the commands

--- src/commandsLoader.ts
+++ src/commandsLoader.ts
@@ -1,18 +1,23 @@
-import fs from 'graceful-fs';
+import { Collection } from 'discord.js';
+import * as fs from 'graceful-fs';
+import { resolve } from 'path';
+import Command from './types/command';
 import logger from './utils/logger';
+import("colors")
+const commands = new Collection<string, Command>()
 
-const commands = new Map<string, any>();
-
-const scanCommands = () => {
-  fs.readdirSync('./commands/').map(async (cmd: string) => {
-    try {
-      const pull = await import(`${__dirname}/commands/${cmd}/`);
-      commands.set(pull.name, pull);
-      logger.log(`[COMMANDS] Scanned ${pull.name.green}`);
-    } catch (e) {
-      logger.warn(`Unable to load command ${cmd}: ${e}`);
-    }
-  });
+const scanCommands = async() => {
+    await fs.readdirSync('./\/commands/').map(async (cmd: string) => {
+      try {
+        if(fs.lstatSync(`${__dirname}/\/commands/${cmd}`).isDirectory()) return
+        const pull = await import(`${__dirname}/\/commands/${cmd}`);
+        commands.set(pull.default.name, pull.default);
+        logger.log(`[COMMANDS] Scanned ${pull.default.name}`.green);
+      } catch (e) {
+        logger.warn(`Unable to load command ${cmd}: ${e}`);
+      }
+    })
+    return commands
 };
 
 export {
--- src/slash.ts
+++ src/slash.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
-import { Client } from 'discord.js';
+import { Client, Guild } from 'discord.js';
 import logger from './utils/logger';
-import { commands, scanCommands } from './commandsLoader';
+import { scanCommands } from './commandsLoader';
 
 if (!process.env.TOKEN) {
   logger.error('[Discord API] No token found.');
@@ -13,26 +13,19 @@ if (!process.env.TOKEN) {
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 const registerSlashCommands = async (client: Client) => {
-  scanCommands();
-  try {
-    client.guilds.cache.forEach(async (guild) => {
-      try {
-        if (!process.env.BOT_ID) {
-          logger.error('[Discord API] No bot ID found.');
-          throw Error('No bot ID found.');
-        }
-        await rest.put(
-          Routes.applicationGuildCommands(process.env.BOT_ID, guild.id),
-          { body: commands },
-        );
-      } catch (e) {
-        logger.error(`[Discord API] Unable to refresh application (/) commands for ${guild.name}: ${e}`);
-      }
-    });
-    logger.log('[Discord API] Successfully registered all commands.');
-  } catch (e) {
-    logger.error(`[Discord API] Failed to reload application (/) commands: ${e}`);
-  }
-};
+  const commands = await scanCommands()
+  logger.log('[Discord API] Refreshing application commands.');
+  client.guilds.cache.forEach(async (guild: Guild) => {
+    if (!process.env.BOT_ID) {
+      logger.error('[Discord API] No bot ID found.');
+      throw Error('No bot ID found.');
+    }
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.BOT_ID, guild.id),
+      { body: commands },
+    );
+  })
+  logger.log('[Discord API] Successfully registered all commands.');
+}
 
 export default registerSlashCommands;
