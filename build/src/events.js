35d8a1a095c37ef9a7c60ebf96fcbd352dfb3ef7
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-06-15 14:58:45 -0400

    refactor: useless build files

--- build/src/events.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const eventsLoader_1 = require("./eventsLoader");
-const resgisterEvents = (client) => {
-    [/*'client', */ 'guild'].map((x) => (0, eventsLoader_1.scanEvents)(client, x));
-};
-exports.default = resgisterEvents;
-//# sourceMappingURL=events.js.map
