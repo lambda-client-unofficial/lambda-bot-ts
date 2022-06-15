1dfb815fb6fe203e171c228c2a5689ae26b98e99
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-06-15 19:03:21 -0400

    refactor: Using listPlugins()

--- src/commands/plugins.ts
+++ src/commands/plugins.ts
@@ -1,6 +1,7 @@
 import ms from 'ms';
 import { Client, CommandInteraction, EmbedBuilder } from 'discord.js';
 import size from '../utils/bytes';
+import listPlugins from '../utils/plugins'
 
 export default {
   name: 'plugins',
@@ -9,28 +10,17 @@ export default {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   run: async (interaction: CommandInteraction, _client: Client) => {
-    const pluginList: { name: any; stargazers: any; watchers: any; issues: string; url: string; size: any; }[] = [];
-    /* const plugins: { name: any; stargazers: any; watchers: any; issues: string; url: string; size: any; }[] = await octokit.request('GET /orgs/{org}/repos', { org: 'lambda-plugins' }) // need fix
-    plugins.forEach((plugin: { name: any; stargazers: any; watchers: any; issues: string; url: string; size: any; }[]) => {
-      pluginList.push({
-        name: plugin?.name,
-        stargazers: plugin?.stargazers_count,
-        watchers: plugin?.watchers_count,
-        issues: `[Issues](${plugin?.issues_url})`,
-        url: `[Repo URL](${plugin?.html_url}) ()`,
-        size: plugin?.size,
-      });
-    }); */
+    const pluginList = await (await listPlugins.listPlugins()).data
     const pluginEmbed = new EmbedBuilder().setTitle('Plugins Informations');
     pluginList.forEach((plugin) => {
       pluginEmbed.addFields([{
         name: plugin.name,
         value:
-          `Stargazers: ${plugin.stargazers}\n
-                Watchers: ${plugin.watchers}\n
-                Issues: ${plugin.issues}\n
-                Size: ${size(plugin.size)}\n
-                ${plugin.url}`,
+          `Stargazers: ${plugin.stargazers_count}\n
+                Watchers: ${plugin.watchers_count}\n
+                Issues: ${plugin.open_issues_count}\n
+                Size: ${size(plugin.size as number)}\n
+                [Repository](${plugin.url})`,
         inline: true,
       }]);
     });
