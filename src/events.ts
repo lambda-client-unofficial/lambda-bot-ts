ac5aa22dc7eea222b332ad6ac11726d73d5a2d59
i-spin <me@i-sp.in>
2022-06-18 12:42:19 -0700

    refactor: merge eventLoader into event file

--- src/events.ts
+++ src/events.ts
@@ -1,8 +1,23 @@
 import { Client } from 'discord.js';
-import { scanEvents } from './eventsLoader';
+import * as fs from 'graceful-fs';
+import * as path from 'path';
+
 import logger from './utils/logger';
 
 import('colors');
+
+const events: any[] = [];
+
+const scanEvents = async (client: Client, dirs: String) => {
+  const scannedEvents = fs.readdirSync(path.join(`${__dirname}/events/${dirs}/`)).filter((d: string) => d.endsWith('js'));
+  scannedEvents.forEach(async (file) => {
+    const evt = await import(`${__dirname}/events/${dirs}/${file}`);
+    const eName = file.split('.')[0];
+    client.on(eName, evt.default.bind(null, client));
+    events.push(evt);
+    logger.log(`[EVENTS] Loaded event ${eName}.`.green);
+  });
+};
 
 const resgisterEvents = (client: Client) => {
   [/* 'client', */'guild'].forEach((x) => {
