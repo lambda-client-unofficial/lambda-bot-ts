ebe56d748e987f1ed5d4ea1b8e08e0215ca2c750
i-spin <me@i-sp.in>
2022-06-18 13:35:22 -0700

    refactor: rename var

--- src/events/guild/interactionCreate.ts
+++ src/events/guild/interactionCreate.ts
@@ -3,7 +3,7 @@ import ms from 'ms';
 import { commands } from '../../slash';
 import embedUtils from '../../utils/embed';
 
-const Timeout = new Set();
+const timeout = new Set();
 
 export default async (client: Client, interaction: CommandInteraction) => {
   if (!commands.has(interaction.commandName)) return;
@@ -11,14 +11,14 @@ export default async (client: Client, interaction: CommandInteraction) => {
   const command = commands.get(interaction.commandName)!;
   try {
     if (command.timeout) {
-      if (Timeout.has(`${interaction.user.id}${command.name}`)) {
+      if (timeout.has(`${interaction.user.id}${command.name}`)) {
         await interaction.reply({ embeds: [embedUtils.error(`You need to wait **${ms(command.timeout, { long: true })}** to use command again`)], ephemeral: true });
       }
     }
     command.run(interaction, client);
-    Timeout.add(`${interaction.user.id}${command.name}`);
+    timeout.add(`${interaction.user.id}${command.name}`);
     setTimeout(() => {
-      Timeout.delete(`${interaction.user.id}${command.name}`);
+      timeout.delete(`${interaction.user.id}${command.name}`);
     }, command.timeout, clearTimeout());
   } catch (e: any) {
     await interaction.reply({ embeds: [embedUtils.error(`An error occured: ${e.toString()}`)] });
