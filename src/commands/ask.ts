bb14f733cd6a0e2c3d2936defc5a968a3d4e073f
i-spin <me@i-sp.in>
2022-06-19 11:14:26 -0700

    feat: ask command
    create thread for questions so dont have to worry about talking over people again

---
+++ src/commands/ask.ts
@@ -0,0 +1,36 @@
+import { Client, CommandInteraction, EmbedBuilder } from 'discord.js';
+import ms from 'ms';
+
+const options = [
+  {
+    name: 'ask',
+    description: 'Creates a thread for your question', // the duplication annoys me
+    type: 1,
+    options: [
+      {
+        name: 'question',
+        description: 'Question',
+        type: 3,
+        required: true,
+      },
+    ],
+  },
+];
+
+export default {
+  name: 'ask',
+  description: 'Creates a thread for your question',
+  options,
+  timeout: ms('5m'),
+  run: async (interaction: CommandInteraction, _client: Client) => {
+    if (!interaction.isChatInputCommand()) return;
+    const embed = new EmbedBuilder()
+      .setTitle(`${interaction.member?.user.username} asked:`)
+      .setDescription(interaction.options.getString('question'))
+      .setColor('Aqua');
+    const sent = await interaction.channel?.send({ embeds: [embed] });
+    sent?.startThread({
+      name: `${interaction.member?.user.username}'s question`,
+    });
+  },
+};
