fe87be01240e81fdfa327feefb0c739baccc48de
i-spin <me@i-sp.in>
2022-06-22 22:27:47 -0700

    feat: commit referencing

--- src/events/guild/messageCreate.ts
+++ src/events/guild/messageCreate.ts
@@ -2,13 +2,19 @@ import { MessageEmbed, Message } from 'discord.js';
 import embedUtils from '../../utils/embed';
 import * as githubUtils from '../../utils/github';
 import logger from '../../utils/logger';
+import { defaultRepo } from '../../../config';
 
 import('colors');
 
+const issueRegex = /#\d+/g;
+const commitRegex = /([a-fA-F0-9]{40})|([a-fA-F0-9]{7})/g; // either 40 characters long alphanumeric or 7 characters alphanumeric
+const fullCommitRegex = /[a-zA-Z0-9](?:[a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38}\/[a-zA-Z0-9](?:[a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38}@(?:([a-fA-F0-9]{40})|([a-fA-F0-9]{7}))/g; // validUsername/validReponame@validCommit
+
 export default async (message: Message) => {
-  if (/#\d+/g.test(message.content)) {
-    message.channel.sendTyping();
-    const match = message.content.match(/#\d+/g)!; // cannot be null because tested above
+  if (message.author.bot) return;
+
+  if (issueRegex.test(message.content)) {
+    const match = message.content.match(issueRegex)!;
     let data;
     try {
       data = await githubUtils.getIssue(null, null, Number(match[0].slice(1)));
@@ -24,5 +30,16 @@ export default async (message: Message) => {
       .setThumbnail(data.user.avatar_url)
       .setColor('AQUA');
     (await message.reply({ embeds: [embed] })).react('🗑');
+  }
+
+  if (commitRegex.test(message.content) || fullCommitRegex.test(message.content)) {
+    try {
+      const match = message.content.match(fullCommitRegex)![0].split('@');
+      message.reply(`https://github.com/${match[0].split('/')[0]}/${match[0].split('/')[1]}/commit/${match[1]}`);
+      message.content.match(commitRegex);
+    } catch (e) {
+      const match = message.content.match(commitRegex)!;
+      message.reply(`https://github.com/${defaultRepo.owner}/${defaultRepo.repo}/commit/${match[0]}`);
+    }
   }
 };
