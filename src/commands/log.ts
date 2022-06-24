79f2a43c91b1082496c39a4d2b700de47efde2f2
i-spin <me@i-sp.in>
2022-06-23 22:54:13 -0700

    feat: log command

---
+++ src/commands/log.ts
@@ -0,0 +1,24 @@
+import { Client, CommandInteraction, MessageEmbed } from 'discord.js';
+import logger from '../utils/logger';
+
+const colorFormat = (log: string) => log
+  .split('\n')
+  .map((line) => {
+    if (line.includes('[SUCCESS]')) return `+ ${line}`;
+    if (line.includes('[ERROR]') || line.includes('[WARN]')) return `- ${line}`;
+    return line;
+  })
+  .join('\n');
+
+export default {
+  name: 'log',
+  description: 'Get last 10 lines of log',
+  run: async (interaction: CommandInteraction, _client: Client) => {
+    if (!interaction.isCommand()) return;
+    const embed = new MessageEmbed()
+      .setTitle('Last 10 lines of log file')
+      .setDescription(`\`\`\`diff\n${colorFormat(await logger.last(10))}\`\`\``)
+      .setColor('AQUA');
+    interaction.reply({ embeds: [embed] });
+  },
+};
